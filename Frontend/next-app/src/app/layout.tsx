18a4b6de53e04c3f4434acedff6545f54f9bece1
NikolayGrozev <nikigr2009@abv.bg>
2025-05-15 21:03:50 +0300

    Installed Frontend dependencies

---
+++ Frontend/next-app/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Tic Tac Toe ",
+  description: "Play Tic-Tac-Toe here!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
